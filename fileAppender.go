cfe59839acd425df37508fab637a2e64e3f9364c
Andy Cui <strawhat2010@live.cn>
2018-11-29 11:35:57 +0800

    internal: added rollover for log files.

--- fileAppender.go
+++ fileAppender.go
@@ -10,6 +10,7 @@ type FileAppender struct {
 type FileAppenderProvider struct {
 	IAppenderProvider
 	File	*os.File
+	filePath	string
 	RolloverSize	int64
 }
 
@@ -26,12 +27,36 @@ func NewFileAppender(file string, rolloverSize int64) *FileAppender {
 	fa.AppenderProvider = &FileAppenderProvider{
 		File: f,
 		RolloverSize: rolloverSize,
+		filePath: file,
 	}
 
 	return fa
 }
 
 func (c *FileAppenderProvider) log(event *LoggingEvent) {
+	fi, err := c.File.Stat()
+	if err == nil && fi.Size() >= c.RolloverSize {
+		// rollover
+		
+		// try the filename exist
+		index := 1
+		for ; ; index++ {
+			path := fmt.Sprintf("%v.%v", c.filePath, index)
+			if _, err := os.Stat(path); os.IsNotExist(err) {
+				// not exist
+				c.File.Close()
+				os.Rename(c.filePath, path)
+				
+				f, err := os.OpenFile(c.filePath, os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+				if err != nil {
+					panic(err)
+				}
+
+				c.File = f
+				break
+			}
+		}
+	}
 	c.File.WriteString(fmt.Sprintln(basicLayout(event)))
 }
 
