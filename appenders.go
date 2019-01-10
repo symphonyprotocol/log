e1f10bc78545c29e4b25c982083c69f5b00838d7
Andy Cui <strawhat2010@live.cn>
2019-01-10 12:09:09 +0800

    internal: removed useless time.Sleep

--- appenders.go
+++ appenders.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"time"
 )
 
 // not configurable now...
@@ -34,14 +33,11 @@ func (b *BaseAppender) Log(event *LoggingEvent) {
 
 func (b *BaseAppender) loop() {
 	for {
-		time.Sleep(time.Millisecond)
 		select {
 		case event := <- b.EventQueue:
 			if b.AppenderProvider != nil {
 				b.AppenderProvider.log(event)
 			}
-		default:
-			// do nothing
 		}
 	}
 }
